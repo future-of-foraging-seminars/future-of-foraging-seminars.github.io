3ffcac08346fb1e328af2111f1f82340b94e4f12
hjmh <hjmhaberkern@gmail.com>
2021-02-19 17:03:35 -0500

    removed spaces

--- contact_process.php
+++ contact_process.php
@@ -1,22 +1,22 @@
 <?php
 
-    $to = "fof.seminar.series@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+  $to = "fof.seminar.series@gmail.com";
+  $from = $_REQUEST['email'];
+  $name = $_REQUEST['name'];
+  $subject = $_REQUEST['subject'];
+  $number = $_REQUEST['number'];
+  $cmessage = $_REQUEST['message'];
 
-    $headers = "From: $from";
+  $headers = "From: $from";
 	$headers = "From: " . $from . "\r\n";
 	$headers .= "Reply-To: ". $from . "\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+  $subject = "You have a message from your Bitmap Photography.";
 
-    $logo = 'img/logo.png';
-    $link = '#';
+  $logo = 'img/logo.png';
+  $link = '#';
 
 	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
